0fb0c455990526a395f320088e49554605043f62
popipuppy <105240901+popipuppy@users.noreply.github.com>
2025-03-12 14:48:51 +0100

    fortsättning funkar

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.send('Get products');
+});
+
+router.post('/', (req, res) => {
+    res.send(req.body);
+});
+
 module.exports = router;
 
