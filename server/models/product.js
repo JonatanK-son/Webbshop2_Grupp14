a7cf343fbebd3b7bd77652eae7601e0215618ea3
Albin Klintman <151128875+AlbinKlintman@users.noreply.github.com>
2025-03-12 13:51:00 +0100

    lektion klar

--- server/models/product.js
+++ server/models/product.js
@@ -1,9 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("product", {
+    return sequelize.define(
+        'product', 
+        {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
+        },
+        created_at: {
+            type: DataTypes.DATE , // Vet inte om det är rätt
+            allowNull: false
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+        imageUrl: {
+            type: DataTypes.STRING(255)
         }
-    })
+    });
 }
