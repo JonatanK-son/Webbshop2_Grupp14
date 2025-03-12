5c81b4c0613f46e9b12ac3c5c3471b6c50715568
popipuppy <105240901+popipuppy@users.noreply.github.com>
2025-03-12 15:08:44 +0100

    fortsättning

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
+const db = require('../models');
 
 router.get('/', (req, res) => {
-    res.send('Get products');
+    db.products.findAll().then((result) => {
+        res.send(result);
+    });
 });
 
 router.post('/', (req, res) => {
     res.send(req.body);
 });
+
+router.put('/', (req, res) => {
+    res.send('Put products');
+});
+
+router.delete('/', (req, res) => {
+    res.send('Delete products');
+});
+
 
 module.exports = router;
 
