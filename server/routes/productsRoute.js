de83b759056f4c5ae3ecc28ac92e508f89f58d04
popipuppy <105240901+popipuppy@users.noreply.github.com>
2025-03-13 10:56:00 +0100

    L2D4 DONE

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -4,12 +4,14 @@ const db validate = require('validate.js');
 
 const constraints = {
     title: {
-        minimum: 2,
-        maximum: 100,
-        tooShort: '^The product name must be at least %{count} characters long.'
-        tooLong: '^The product can not be longer than &{count} characters long.'
+        length: {
+            minimum: 2,
+            maximum: 100,
+            tooShort: '^The product name must be at least %{count} characters long.'
+            tooLong: '^The product can not be longer than &{count} characters long.'
+        }
     }
-}
+};
 
 router.get('/', (req, res) => {
     db.products.findAll().then((result) => {
@@ -18,20 +20,32 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    
-    db.products.create(req.body).then(result => {
+    const product = req.body;
+    const invalidData = validate(product, contraints);
+    if(invalidData) {
+        res.status(400).json(invalidData);
+    } else {
+        db.products.create(product).then(result => {
         res.send(result);
-    });
+    }); 
+    }
 });
 
 router.put('/', (req, res) => {
-    db.products
-     .update(req.body, {
-        where: {id: req.body.id } 
-    })
-    .then(result => {
-        res.send(result);
-    })
+    const product = req.body;
+    const invalidData = validate(product, contraints);
+    const id = post.id
+    if(invalidData || !id ) {
+        res.status(400).json(invalidData || 'Id is mandotary');
+    } else {
+        db.products
+        .update(product, {
+           where: {id: product.id } 
+       })
+       .then(result => {
+           res.send(result);
+       });
+    }
 });
 
 router.delete('/', (req, res) => {
